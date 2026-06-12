edd23f56667f67d4e91047bcb11584294a685e27
kyeongminn <sally0264@naver.com>
2026-06-12 11:32:35 +0900

    [fix]: splash_screen.dart 되돌리기

--- lib/features/splash/presentation/splash_screen.dart
+++ lib/features/splash/presentation/splash_screen.dart
@@ -1,29 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:ssairen/core/router/app_router.dart';
-import 'package:ssairen/core/router/route_paths.dart';
+import 'package:ssairen/core/widgets/placeholder_screen.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    // TODO: 실제 스플래시 로직(권한 체크, 로그인 여부 등) 구현 후 제거
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      Navigator.of(context)
-          .pushReplacement(AppRouter.fadeRoute(RoutePaths.home));
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: CircularProgressIndicator()),
+    return const PlaceholderScreen(
+      title: 'SSAIREN',
+      description: '스플래시 화면',
     );
   }
 }
